1c9051d48586be4c0fbe7df28353d034a55121ab
naufalfarel <naufalfaerel@gmail.com>
2026-04-27 22:14:40 +0700

    feat: tambah maker lokasi masjid

--- kelompok-1-kelas-b/maker.js
+++ kelompok-1-kelas-b/maker.js
@@ -1,10 +1,20 @@
 const lokasiMarker = [
   {
-    nama: "rumah anggota kelompok 1",
+    nama: "Rumah Anggota Kelompok 1 (Farel)",
     lat: 5.560591,
     lng: 95.33627,
     kategori: "Rumah",
     anggota: "Farel",
-    catatan: "Lokasi Rumah anggota keompok 1-B.",
+    catatan:
+      "Lokasi rumah anggota Kelompok 1 Kelas B yang digunakan sebagai contoh marker.",
+  },
+  {
+    nama: "Masjid Jami' Darussalam (USK)",
+    lat: 5.570809,
+    lng: 95.37143,
+    kategori: "Fasilitas Kampus",
+    anggota: "Farel",
+    catatan:
+      "Masjid utama di lingkungan Universitas Syiah Kuala, digunakan untuk kegiatan ibadah mahasiswa.",
   },
 ];
