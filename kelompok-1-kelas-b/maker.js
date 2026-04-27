e48d77d1db48d5cf1dea05c6a6f2ad4419108341
naufalfarel <naufalfaerel@gmail.com>
2026-04-27 21:28:23 +0700

    feat: tambah marker

--- kelompok-1-kelas-b/maker.js
+++ kelompok-1-kelas-b/maker.js
@@ -1,1 +1,10 @@
-const dataLokasiTim = [];
+const dataLokasiTim = [
+  {
+    nama: "rumah anggota kelompok",
+    lat: 5.560591,
+    lng: 95.33627,
+    kategori: "Rumah",
+    anggota: "Farel",
+    catatan: "Lokasi Rumah anggota keompok 1-B.",
+  },
+];
